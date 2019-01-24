152838cddb0635954ff01f0c5f98b067aec6e687
AntonNikitin <kb.nikitinav@mail.ru>
2019-01-24 19:34:40 +0300

    первая версия с программным ШИМ готова, задержка переключения 70с

--- AVR/uad/uad/main.c
+++ AVR/uad/uad/main.c
@@ -14,9 +14,10 @@
 //#include <avr/io.h>
 struct_GPIO LED1, LED2, LED3, LED4, LED5, LED6, LED7, LED8;
 
-float alpha = 0.2, float_ADC3 = 0, float_ADC3_last = 0;
+float alpha = 0.1, float_ADC3 = 0, float_ADC3_last = 0;
 
-int count = 0, limit = 0;
+int count = 0, pwm_out_1 = 0, pwm_out_2 = 0 , limit = 0;
+unsigned long delay = 320000, timer = 0;
 
 int main(void)
 {
@@ -45,158 +46,67 @@ int main(void)
 	ADCSRA = 0b11100111;	// ADC=on, multiple, prescal = 128
 	// 0 -> ADEN, 1 -> ADSC, 2 -> ADFR, 3 -> ADIF, 4 -> ADIE, 5 -> ADPS2, 6 -> ADPS1, 7 -> ADPS0
 	
-	
 	pinMode(B1, OUTPUT);
 	pinMode(B2, OUTPUT);
 	
     while (1) 
     {	
-
 		delay_counters(8);
 		//delay_counters(80);
 		
 		float_ADC3 = ADCH*alpha + float_ADC3_last*(1-alpha);
 		
+		digitalWrite(LED1, LOW);
+		digitalWrite(LED2, LOW);
+		digitalWrite(LED3, LOW);
+		digitalWrite(LED4, LOW);
+		digitalWrite(LED5, LOW);
+		digitalWrite(LED6, LOW);
+		digitalWrite(LED7, LOW);
+		digitalWrite(LED8, LOW);
+		
 		if (float_ADC3<=10)	{
-			digitalWrite(LED1, LOW);
-			digitalWrite(LED2, LOW);
-			digitalWrite(LED3, LOW);
-			digitalWrite(LED4, LOW);
-			digitalWrite(LED5, LOW);
-			digitalWrite(LED6, LOW);
-			digitalWrite(LED7, LOW);
-			digitalWrite(LED8, LOW);
 			limit = 0;
 		}
 		
 		if ( (float_ADC3>10)&(float_ADC3<=30) )	{
 			digitalWrite(LED1, HIGH);
-			digitalWrite(LED2, LOW);
-			digitalWrite(LED3, LOW);
-			digitalWrite(LED4, LOW);
-			digitalWrite(LED5, LOW);
-			digitalWrite(LED6, LOW);
-			digitalWrite(LED7, LOW);
-			digitalWrite(LED8, LOW);
-			limit = 30;
+			limit = 1;
 		}
 		
 		if ( (float_ADC3>30)&(float_ADC3<=60) )	{
-			//digitalWrite(LED1, HIGH);
-			digitalWrite(LED1, LOW);
 			digitalWrite(LED2, HIGH);
-			digitalWrite(LED3, LOW);
-			digitalWrite(LED4, LOW);
-			digitalWrite(LED5, LOW);
-			digitalWrite(LED6, LOW);
-			digitalWrite(LED7, LOW);
-			digitalWrite(LED8, LOW);
-			limit = 60;
+			limit = 2;
 		}
 		
 		if ( (float_ADC3>60)&(float_ADC3<=90) )	{
-			//digitalWrite(LED1, HIGH);
-			//digitalWrite(LED2, HIGH);
-			digitalWrite(LED1, LOW);
-			digitalWrite(LED2, LOW);
 			digitalWrite(LED3, HIGH);
-			digitalWrite(LED4, LOW);
-			digitalWrite(LED5, LOW);
-			digitalWrite(LED6, LOW);
-			digitalWrite(LED7, LOW);
-			digitalWrite(LED8, LOW);
-			limit = 90;
+			limit = 3;
 		}
 		
 		if ( (float_ADC3>90)&(float_ADC3<=120) )	{
-			//digitalWrite(LED1, HIGH);
-			//digitalWrite(LED2, HIGH);
-			//digitalWrite(LED3, HIGH);
-			digitalWrite(LED1, LOW);
-			digitalWrite(LED2, LOW);
-			digitalWrite(LED3, LOW);
 			digitalWrite(LED4, HIGH);
-			digitalWrite(LED5, LOW);
-			digitalWrite(LED6, LOW);
-			digitalWrite(LED7, LOW);
-			digitalWrite(LED8, LOW);
-			limit = 120;
+			limit = 4;
 		}
 		
 		if ( (float_ADC3>120)&(float_ADC3<=150) )	{
-			//digitalWrite(LED1, HIGH);
-			//digitalWrite(LED2, HIGH);
-			//digitalWrite(LED3, HIGH);
-			//digitalWrite(LED4, HIGH);
-			digitalWrite(LED1, LOW);
-			digitalWrite(LED2, LOW);
-			digitalWrite(LED3, LOW);
-			digitalWrite(LED4, LOW);
 			digitalWrite(LED5, HIGH);
-			digitalWrite(LED6, LOW);
-			digitalWrite(LED7, LOW);
-			digitalWrite(LED8, LOW);
-			limit = 150;
+			limit = 5;
 		}
 		
 		if ( (float_ADC3>150)&(float_ADC3<=180) )	{
-			/*
-			digitalWrite(LED1, HIGH);
-			digitalWrite(LED2, HIGH);
-			digitalWrite(LED3, HIGH);
-			digitalWrite(LED4, HIGH);
-			digitalWrite(LED5, HIGH);
-			*/
-			digitalWrite(LED1, LOW);
-			digitalWrite(LED2, LOW);
-			digitalWrite(LED3, LOW);
-			digitalWrite(LED4, LOW);
-			digitalWrite(LED5, LOW);
 			digitalWrite(LED6, HIGH);
-			digitalWrite(LED7, LOW);
-			digitalWrite(LED8, LOW);
-			limit = 180;
+			limit = 6;
 		}
 		
 		if ( (float_ADC3>180)&(float_ADC3<=210) )	{
-			/*
-			digitalWrite(LED1, HIGH);
-			digitalWrite(LED2, HIGH);
-			digitalWrite(LED3, HIGH);
-			digitalWrite(LED4, HIGH);
-			digitalWrite(LED5, HIGH);
-			digitalWrite(LED6, HIGH);
-			*/
-			digitalWrite(LED1, LOW);
-			digitalWrite(LED2, LOW);
-			digitalWrite(LED3, LOW);
-			digitalWrite(LED4, LOW);
-			digitalWrite(LED5, LOW);
-			digitalWrite(LED6, LOW);
 			digitalWrite(LED7, HIGH);
-			digitalWrite(LED8, LOW);
-			limit = 210;
+			limit = 7;
 		}
 		
 		if (float_ADC3>210)	{
-			/*
-			digitalWrite(LED1, HIGH);
-			digitalWrite(LED2, HIGH);
-			digitalWrite(LED3, HIGH);
-			digitalWrite(LED4, HIGH);
-			digitalWrite(LED5, HIGH);
-			digitalWrite(LED6, HIGH);
-			digitalWrite(LED7, HIGH);
-			*/
-			digitalWrite(LED1, LOW);
-			digitalWrite(LED2, LOW);
-			digitalWrite(LED3, LOW);
-			digitalWrite(LED4, LOW);
-			digitalWrite(LED5, LOW);
-			digitalWrite(LED6, LOW);
-			digitalWrite(LED7, LOW);
 			digitalWrite(LED8, HIGH);
-			limit = 255;
+			limit = 8;
 		}
 		
 		float_ADC3_last = float_ADC3;
@@ -204,19 +114,39 @@ int main(void)
 		//	PB1 -> PWM2, timer1, canal A
 		//	PB2 -> PWM1, timer1, canal B
 		
-		
-		if (count>255)	{
+		if (count>7)	{
 			count = 0;
+			
+			if (timer<=delay)	{
+				pwm_out_1 = limit;	
+				pwm_out_2 = 0;
+			}
+			if ( (timer>delay)&(timer<=2*delay) )	{
+				pwm_out_2 = limit;
+				pwm_out_1 = 0;
+			}
+			if (timer>2*delay)	{
+				timer = 0;
+			}
 		}
-		if ( limit <= count )	{
+		
+		if ( pwm_out_1 <= count )	{
 			digitalWrite(B1, LOW);
 		}
 		else	{
 			digitalWrite(B1, HIGH);
 		}
+		
+		
+		if ( pwm_out_2 <= count )	{
+			digitalWrite(B2, LOW);
+		}
+		else	{
+			digitalWrite(B2, HIGH);
+		}
+		
 		count++;
-		
-		
+		timer++;
     }
 }
 
